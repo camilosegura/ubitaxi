2421ff51c53bdfa75c530b3491517541b694c0f5
Camilo Segura <camilo@camilof>
2012-10-07 17:18:16 -0500

    se agrega una accion que maneja el almacenamiento de los datos enviados desde el movil del carro

--- protected/modules/destinos/controllers/DefaultController.php
+++ protected/modules/destinos/controllers/DefaultController.php
@@ -6,4 +6,18 @@ class DefaultController extends Controller
 	{
 		$this->render('index');
 	}
+        
+        public function actionSavePos() {
+            
+            $seguimiento = new SeguimientoController('seguimiento',$this->module);
+            $idPos = $seguimiento->actionCreateCar();
+            if($idPos){
+                $id_telefono = $_GET["Seguimiento"]["id_telefono"];
+                $vehiculo = new VehiculoController('vehiculo', $this->module);
+                if($vehiculo->actionUpdateCar($idPos, $id_telefono)){
+                    $rsp["success"] = true;
+                    echo json_encode($rsp);
+                }
+            }
+        }
 }
