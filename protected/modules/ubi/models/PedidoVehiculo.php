7cc64aec4ae0a78381fba560f5d675132c2d9165
Camilo Segura <camilo@camilof>
2012-10-11 13:34:07 -0500

    se crea modelo para pedidos-vehiculos

---
+++ protected/modules/ubi/models/PedidoVehiculo.php
@@ -0,0 +1,111 @@
+<?php
+
+/**
+ * This is the model class for table "{{pedido_vehiculo}}".
+ *
+ * The followings are the available columns in table '{{pedido_vehiculo}}':
+ * @property integer $id
+ * @property integer $id_pedido
+ * @property integer $id_vehiculo
+ * @property string $time
+ * @property string $valor
+ * @property string $unidades
+ * @property string $direccion_destino
+ * @property string $latitud
+ * @property string $longitud
+ */
+class PedidoVehiculo extends CActiveRecord
+{
+	/**
+	 * Returns the static model of the specified AR class.
+	 * @param string $className active record class name.
+	 * @return PedidoVehiculo the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+	
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return '{{pedido_vehiculo}}';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('id_pedido, id_vehiculo, time, valor, unidades, direccion_destino, latitud, longitud', 'required'),
+			array('id_pedido, id_vehiculo', 'numerical', 'integerOnly'=>true),
+			array('time, latitud, longitud', 'length', 'max'=>255),
+			array('valor', 'length', 'max'=>60),
+			array('unidades', 'length', 'max'=>6),
+			array('direccion_destino', 'length', 'max'=>500),
+			// The following rule is used by search().
+			// Please remove those attributes that should not be searched.
+			array('id, id_pedido, id_vehiculo, time, valor, unidades, direccion_destino, latitud, longitud', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => 'ID',
+			'id_pedido' => 'Id Pedido',
+			'id_vehiculo' => 'Id Vehiculo',
+			'time' => 'Time',
+			'valor' => 'Valor',
+			'unidades' => 'Unidades',
+			'direccion_destino' => 'Direccion Destino',
+			'latitud' => 'Latitud',
+			'longitud' => 'Longitud',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// Warning: Please modify the following code to remove attributes that
+		// should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id',$this->id);
+		$criteria->compare('id_pedido',$this->id_pedido);
+		$criteria->compare('id_vehiculo',$this->id_vehiculo);
+		$criteria->compare('time',$this->time,true);
+		$criteria->compare('valor',$this->valor,true);
+		$criteria->compare('unidades',$this->unidades,true);
+		$criteria->compare('direccion_destino',$this->direccion_destino,true);
+		$criteria->compare('latitud',$this->latitud,true);
+		$criteria->compare('longitud',$this->longitud,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+}
