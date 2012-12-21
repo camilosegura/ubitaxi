11df270ff7050b775d46695d4c1b0d8c769369ca
camilo <camilo@camilo-ubuntu>
2012-12-21 01:23:32 -0500

    se agrega acrchivo de configuracion de jqm, se quitan las transiciones para evirae el error de jump flip al cargar dada página

---
+++ apps/android/pasajero/assets/www/js/jquery.mobile.config.js
@@ -0,0 +1,10 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$(document).one("mobileinit", function() {
+    // Setting default page transition to slide
+    $.mobile.defaultPageTransition = 'none';
+    $.mobile.buttonMarkup.hoverDelay = 0;
+});
+
