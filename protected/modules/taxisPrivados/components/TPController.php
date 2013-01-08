4c382d436d2eceef533117abd74cf8c766e89616
camilo <camilo@camilo-ubuntu>
2013-01-08 01:50:18 -0500

    se cargan en variables globales los roles y las empresas que tiene cada usuario

--- protected/modules/taxisPrivados/components/TPController.php
+++ protected/modules/taxisPrivados/components/TPController.php
@@ -29,20 +29,36 @@ class TPController extends Controller {
      * for more details on how to specify this property.
      */
     public $breadcrumbs = array();
-    public $empresaId;
-    public $empresaNombre;
-    public $roles;
+
+    /**
+     *
+     * @var type array se guarda un arreglo con las empresas relacionadas a los usuarios, 
+     * las key del array son los ID de las empresas
+     */
+    public $empresas = array();
+
+    /**
+     *
+     * @var type Array con los roles del usuario
+     */
+    public $roles = array();
 
     //put your code here
     public function init() {
         Yii::app()->theme = 'taxisPrivados';
-        $this->roles = array_keys(Rights::getAssignedRoles(Yii::app()->user->Id));
-        Yii::app()->user->checkAccess('account');
-        if (Yii::app()->user->isSuperuser) {
-            $this->empresaId = 0;
-        } else {
-            
-        }                
+        $emp = array();
+        if (!Yii::app()->user->isGuest) {
+            $this->roles = array_keys(Rights::getAssignedRoles(Yii::app()->user->Id));
+            if (in_array('AdminOperador', $this->roles)) {
+                $emp = Empresa::model()->findAll();
+            } else {
+                $emp = Empresa::model()->with('usuario')->findAll('usuario.id_usuario=:id_usuario', array(':id_usuario' => Yii::app()->user->id));
+            }
+            foreach ($emp as $key => $empresa) {
+                $this->empresas[$empresa->id] = $empresa->nombre;
+            }            
+        }
+
         parent::init();
     }
 
