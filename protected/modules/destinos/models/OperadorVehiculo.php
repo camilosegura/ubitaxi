c51072dd7f0abf376912e9ae5725be22a06b16db
camilo <camilo@camilo-ubuntu>
2013-01-08 01:52:51 -0500

    se agrega relacion con los vehiculos

--- protected/modules/destinos/models/OperadorVehiculo.php
+++ protected/modules/destinos/models/OperadorVehiculo.php
@@ -52,6 +52,7 @@ class OperadorVehiculo extends CActiveRecord
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
 		return array(
+                    'vehiculo'=>array(self::BELONGS_TO, 'Vehiculo', 'id_vehiculo'),
 		);
 	}
 
