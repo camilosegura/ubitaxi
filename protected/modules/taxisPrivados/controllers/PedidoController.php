b7c59f33f15adddfdbf4244f905f62fb488ef54f
camilo <camilo@camilo-ubuntu>
2013-01-10 13:09:35 -0500

    se cran acciones para aceptar e iniciar el pedido por parte del conductor

--- protected/modules/taxisPrivados/controllers/PedidoController.php
+++ protected/modules/taxisPrivados/controllers/PedidoController.php
@@ -11,6 +11,7 @@ class PedidoController extends TPController {
     }
 
     public function actionNuevo() {
+        $hasPedido['success'] = false;
         if (isset($_POST['empresa'])) {
             $direccion = Direccion::model()->findByPk($_POST['direccionSalida']);
 
@@ -37,9 +38,14 @@ class PedidoController extends TPController {
             $emPedido->hora_inicio = date('Y-m-d') . " {$_POST['horaSalida']}:00";
             $emPedido->direcciones = serialize($_POST['pasajeros']);
             $emPedido->save();
+            
+            $hasPedido['success'] = true;
+            $hasPedido['id'] = $pedido->id;
             //var_dump($emPedido->getErrors());
         }
-        $this->render('nuevo', array('empresas' => $this->empresas));
+        $this->render('nuevo', array(
+            'empresas' => $this->empresas,
+            'pedido' => $hasPedido));
     }
 
     private function pedidoAssign($disponibles, $idPedido) {
@@ -59,9 +65,78 @@ class PedidoController extends TPController {
     }
 
     public function actionVer() {
+        
         $this->render('ver');
     }
+    public function actionAceptar() {
+        $pedido = Pedido::model()->findByPk($_GET['id_pedido']);
+        if ($pedido->id_estado === '0') {
 
+            $vehiculo = Vehiculo::model()->findByPk($_GET['id_vehiculo']);
+            $vehiculo->estado = 1;
+            if ($vehiculo->save()) {
+                $pedido->id_estado = 1;
+                $pedido->tiempo_llegar = 0;
+                $pedido->save();
+                Vehiculo::model()->updateAll(array('id_pedido' => 0), 'id_pedido=:id_pedido AND estado = 0', array(':id_pedido' => $_GET['id_pedido']));
+                $empresaPedido = EmpresaPedido::model()->find('id_pedido=:id_pedido', array(':id_pedido'=>$_GET['id_pedido']));
+                $rsp['direccion'] = $pedido->direccion_origen;
+                $rsp["lat"] = $pedido->latitud;
+                $rsp["lng"] = $pedido->longitud;
+                $rsp['msg'] = 'Hora de recogida '.$empresaPedido->hora_inicio.'.  El pasajero lo espera en ' . $pedido->direccion_origen;
+                $rsp['success'] = true;
+                $rsp['hora_inicio'] = $empresaPedido->hora_inicio;
+               
+                echo json_encode($rsp);
+                return true;
+            }
+        }
+        $rsp['success'] = false;
+        $rsp['msg'] = 'El pedido ya fue asignado';
+
+        echo json_encode($rsp);
+        //echo 'sdsd';
+        //return false;
+    }
+    public function actionIniciar() {
+
+        $pedido = Pedido::model()->findByPk($_GET["id_pedido"]);
+                
+        if (!is_null($pedido)) {
+
+            $pedidoVehiculo = new PedidoVehiculo();
+            $pedidoVehiculo->id_pedido = $_GET["id_pedido"];
+            $pedidoVehiculo->id_vehiculo = $_GET["id_vehiculo"];
+            $pedidoVehiculo->time = time();
+            $pedidoVehiculo->save();
+
+            $model = Pedido::model()->findByPk($_GET["id_pedido"]);
+            $model->id_estado = 3;
+            $model->save();
+            
+            $empresaPedido = EmpresaPedido::model()->find('id_pedido=:id_pedido', array(':id_pedido'=>$_GET["id_pedido"]));
+            $empresaPedido->personas_vehiculo = $_GET['pasajeros'];
+            $empresaPedido->save();
+            
+            $direccionesId = unserialize($empresaPedido->direcciones);
+            $direcciones = Direccion::model()->findAll('id IN ('.implode(',', $direccionesId).')');
+            foreach ($direcciones as $key => $direccion) {
+                $rsp['direcciones'][$direccion->id]['direccion'] = $direccion->direccion;
+                $rsp['direcciones'][$direccion->id]['latitud'] = $direccion->latitud;
+                $rsp['direcciones'][$direccion->id]['longitud'] = $direccion->longitud;
+                $rsp['direcciones'][$direccion->id]['id_usuario'] = $direccion->id_user;
+            }
+            $rsp['dir'] = $direcciones[0]->latitud;
+            
+            $rsp["success"] = true;
+            $rsp["msg"] = "Iniciando el recorrido";
+            $rsp["id"] = $pedidoVehiculo->id;
+        } else {
+            $rsp["success"] = false;
+            $rsp["msg"] = "El pedido no es correcta";
+        }
+        echo json_encode($rsp);
+    }
     // Uncomment the following methods and override them if needed
 
     public function filters() {
