a4802cb526130b7fe0392c0c419a2106fd86fc57
camilo <camilo@camilo-ubuntu>
2013-01-28 13:50:12 -0500

    se adiciona js necesario para que funcione el mapa

--- protected/modules/taxisPrivados/views/default/_mapa.php
+++ protected/modules/taxisPrivados/views/default/_mapa.php
@@ -4,5 +4,16 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+/* @var $this EmpresaController */
+/* @var $model Empresa */
+/* @var $form CActiveForm */
+$baseUrl = Yii::app()->baseUrl;
+$cs = Yii::app()->getClientScript();
+$cs->registerScriptFile('http://maps.google.com/maps/api/js?sensor=true', CClientScript::POS_END);
+$cs->registerScriptFile($baseUrl . '/js/geolocation.js', CClientScript::POS_END);
+$cs->registerScript('script', <<<JS
+    initialize();
+JS
+        , CClientScript::POS_READY);
 ?>
 <div  id="map_canvas" class="span8"></div>
