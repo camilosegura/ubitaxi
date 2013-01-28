4652057a9e278475631395c30ce8ae493824ebfc
camilo <camilo@camilo-ubuntu>
2013-01-28 13:51:11 -0500

    se quita js que habia para que funcionara el mapa

--- protected/modules/taxisPrivados/views/usuario/_formularioPasajero.php
+++ protected/modules/taxisPrivados/views/usuario/_formularioPasajero.php
@@ -1,15 +1,5 @@
 <?php
-/* @var $this EmpresaController */
-/* @var $model Empresa */
-/* @var $form CActiveForm */
-$baseUrl = Yii::app()->baseUrl;
-$cs = Yii::app()->getClientScript();
-$cs->registerScriptFile('http://maps.google.com/maps/api/js?sensor=true', CClientScript::POS_END);
-$cs->registerScriptFile($baseUrl . '/js/geolocation.js', CClientScript::POS_END);
-$cs->registerScript('script', <<<JS
-    initialize();
-JS
-        , CClientScript::POS_READY);
+
 ?>
 <div class="form span4">
 
