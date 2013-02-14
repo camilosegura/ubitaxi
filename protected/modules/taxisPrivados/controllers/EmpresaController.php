40e9ee666d02258499a52ebe6c5c886ac6cc11ad
camilo <camilo@camilo-ubuntu>
2013-02-13 22:54:08 -0500

    se ponen accciones para listar las empresas

--- protected/modules/taxisPrivados/controllers/EmpresaController.php
+++ protected/modules/taxisPrivados/controllers/EmpresaController.php
@@ -163,7 +163,12 @@ class EmpresaController extends TPController {
             'model' => $model,
         ));
     }
-
+    public function actionListar() {
+        $model = Empresa::model()->findAll();
+        $this->render('listar', array(
+            'model' => $model,
+        ));
+    }
     /**
      * Returns the data model based on the primary key given in the GET variable.
      * If the data model is not found, an HTTP exception will be raised.
