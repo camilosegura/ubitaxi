2b1393afe2cc5ae4096f9b7b04811b28e63ccf91
camilo <camilo@camilo-ubuntu>
2013-02-22 06:06:36 -0500

    se adiciona el estado del pedido

--- protected/modules/taxisPrivados/controllers/ReporteController.php
+++ protected/modules/taxisPrivados/controllers/ReporteController.php
@@ -3,31 +3,34 @@
 class ReporteController extends TPController {
 
     public function actionIndex() {
-        $selected = array('fechaInicio'=>'', 'fechaFin'=>'', 'empresa'=>'', 'vehiculo'=>'');
+        $selected = array('fechaInicio' => '', 'fechaFin' => '', 'empresa' => '', 'vehiculo' => '', 'estado' => '');
         if (isset($_POST['empresa'])) {
             $selected['empresa'] = $_POST['empresa'];
             $selected['fechaInicio'] = $_POST['fechaInicio'];
             $selected['fechaFin'] = $_POST['fechaFin'];
             $selected['vehiculo'] = $_POST['vehiculo'];
-            
+            $selected['estado'] = $_POST['estado'];
+
             $query = 'hora_inicio >=:hora_inicio AND hora_fin <=:hora_fin';
             $params[':hora_inicio'] = $_POST['fechaInicio'];
-            $params[':hora_fin'] = $_POST['fechaFin'].' 23:59:59';
-            
-            if($_POST['empresa'] != '')
-            {
+            $params[':hora_fin'] = $_POST['fechaFin'] . ' 23:59:59';
+
+            if ($_POST['empresa'] != '') {
                 $query .=' AND empresa.id=:id_empresa';
                 $params[':id_empresa'] = $_POST['empresa'];
             }
-            if($_POST['vehiculo'] != '')
-            {
+            if ($_POST['vehiculo'] != '') {
                 $query .=' AND vehiculo.id=:id_vehiculo';
                 $params[':id_vehiculo'] = $_POST['vehiculo'];
             }
-                        
-            $reservas = PedidoReserva::model()->with('direccionesCompletas', 'empresa', 'vehiculo', 'valor')->findAll($query, $params);
+            if ($_POST['estado'] != '') {
+                $query .=' AND pedido.id_estado=:id_estado';
+                $params[':id_estado'] = $_POST['estado'];
+            }
+
+            $reservas = PedidoReserva::model()->with('peticion', 'pedido', 'direccionesCompletas', 'empresa', 'vehiculo', 'valor')->findAll($query, $params);
         } else {
-            $reservas = PedidoReserva::model()->with('direccionesCompletas', 'empresa', 'vehiculo', 'valor')->findAll();
+            $reservas = PedidoReserva::model()->with('peticion', 'pedido', 'direccionesCompletas', 'empresa', 'vehiculo', 'valor')->findAll();
         }
         foreach ($reservas as $key => $reserva) {
 
@@ -37,6 +40,8 @@ class ReporteController extends TPController {
             $pedidos[$reserva->id_pedido]['valorEmpresa'] = $reserva->valor->valor_empresa;
             $pedidos[$reserva->id_pedido]['valorVehiculo'] = $reserva->valor->valor_vehiculo;
             $pedidos[$reserva->id_pedido]['ruta'] = $reserva->valor->ruta;
+            $pedidos[$reserva->id_pedido]['estado'] = $reserva->pedido->id_estado;
+            $pedidos[$reserva->id_pedido]['peticion'] = $reserva->peticion->id;
 
             foreach ($reserva->direccionesCompletas as $keyDir => $direccion) {
                 if ($direccion->id_user != '0') {
@@ -73,6 +78,14 @@ class ReporteController extends TPController {
             $rsp['success'] = true;
             echo json_encode($rsp);
         }
+    }
+
+    public function actionAExcel() {
+        header("Content-type: application/vnd.ms-excel; name='excel'");
+        header("Content-Disposition: filename=reporte.xls");
+        header("Pragma: no-cache");
+        header("Expires: 0");
+        echo $_POST["contenido"];
     }
 
     // Uncomment the following methods and override them if needed
